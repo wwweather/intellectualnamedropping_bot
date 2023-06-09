06fc42ca88f3320b55b8011a5912c04743b32b15
wwweather <110007453+wwweather@users.noreply.github.com>
2023-06-09 13:16:43 +0800

    Update bot.py (not tested)

--- bot.py
+++ bot.py
@@ -1,11 +1,34 @@
 import telebot
 import config
+#import requests
+from random import choice
+from pathlib import Path
+import re
 from telebot import types 
 
-telegram_token = config.telegrambot_token
-bot = telebot.TeleBot(telegram_token)
+bot_token = "<EXISTING_BOT_TOKEN>"
+bot = telebot.TeleBot(bot_token)
 
-@bot.message_handler(commands=['start','go', 'help'])
+def getNextAvailableIndex(availableIndices, indicesSoFar):
+    availableIndices.remove(indicesSoFar[-1])
+    if len(availableIndices) > 0:
+        return sorted(availableIndices)[0]
+    else:
+        return -1
+
+def loadNamesFromFile():
+    try:
+        with open("names.txt") as file:
+            names = [line.strip() for line in file]
+    except FileNotFoundError:
+        raise ValueError("Nameslist file not found.")
+    if len(set(names)) != len(names):
+        raise ValueError("Duplicate entries in nameslist file.")
+            return names
+    except FileNotFoundError:
+        raise ValueError("Nameslist file not found.")
+        
+@bot.message_handler(commands=['start', 'go', 'help', 'move'])
 def start(message):
     bot.send_message(message.chat.id, text=f'Привет, {message.from_user.username}, поиграем?')
     markup_inline = types.InlineKeyboardMarkup()
@@ -18,14 +41,35 @@ def start(message):
 def go_to(callback):
     if callback.data == "start_the_game":
         bot.send_message(callback.from_user.id, text='Первый ход за тобой!')
-    # bot.answer_callback_query(callback_query_id=callback.id)
 
 @bot.message_handler(content_types=['text'])  # Ограничение по типу информации на входе
-def handle_text(message):
-    bot.reply_to(message, 'тривиальная обработка хода')
+def move(chat_id, message):
+    global lastSelectedIndex
+    if message in names:
+        if lastSelectedIndex >= 0:
+            nextIndex = getNextAvailableIndex([lastSelectedIndex], [i for i in range(len(names)) if i != lastSelectedIndex])
+            if nextIndex < 0:
+                raise ValueError("Все имена уже назывались! Пора сказать что-то новое, господин-философ!")
+            this_move = names[nextIndex] + "\n"
+            bot.send_message(callback.from_user.id, text=this_move)
+        elif: message == '/move':
+            bot.send_message(message.chat.id, text=f'Человек, ознакомься с правилами игры.')   
+        else:
+            firstSelection = choice(words)
+            this_move = firstSelection + "\n"
+            bot.send_message(callback.from_user.id, text=this_move)
+        lastSelectedIndex = len(words)-1
+    else:
+        print("О ком вы вообще говорите?")
 
 @bot.message_handler(content_types=['photo', 'video', 'audio'])  # Ограничение по типу информации на входе
 def handle_all(message):
     bot.reply_to(message, "Нет, бот не принимает фотографии философов и записи их лекций в формате игры. Распознавать такие входные данные — ваша задача, господин-интеллектуал.")
 
 bot.polling(none_stop=True) # Для нон-стоп работы
+        
+if __name__ == '__main__':
+    chat_id = int(input(f'{message.from_user.username}:')
+    message = input("Используйте интерактив:")
+else:
+    loadNamesFromFile()
